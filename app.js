9a923e5f4710c1ec9b95fd5efcf639def9ac8ddc
Janaki04 <telugujanaki412@gmail.com>
2022-02-20 12:29:40 +0530

    first

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const router = require("./routing");
 
 const app = express();  
 
+const PORT = process.env.PORT || 8000;
+
 app.use(cors())
 app.use("/api/v1", router);
 
-app.listen(8000, ()=>{
+app.listen(PORT, ()=>{
     console.log("Application has started successfully");
 })
